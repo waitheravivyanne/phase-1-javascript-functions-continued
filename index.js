2f98a5702a297b0757e4ca20b80cd83d7466d20d
vivian waithera <vivyannekori@gmail.com>
2023-06-14 18:09:46 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 // code your solution here
+function saturdayFun(activity = "roller-skate"){
+    return 'This Saturday, I want to '+activity+'!';
+}
+
+// function saturdayFun(activity = 'bathe my dog'){
+//     return ("This Saturday,I want to bathe my dog!")
+
+// }
+function mondayWork(activity = 'go to the office'){
+    return 'This Monday, I will '+ activity + '.';
+}
+
+function wrapAdjective(tool = '*'){
+    return function(adjective = 'special'){
+        return 'You are ' + tool + adjective + tool + '!';
+    }
+}
